51120d07179162bac47511c757f5c9ba201ade92
Diego <di3g0d0ming05@gmail.com>
2019-03-25 17:09:59 -0300

    commit inicial

---
+++ app/src/main/java/br/com/blogapp/Ui/RegisterActivity.java
@@ -0,0 +1,15 @@
+package br.com.blogapp.Ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.blogapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
